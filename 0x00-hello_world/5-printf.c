545911c7df542ffb3af78744795e7df753150de8
Listowel-CodescodeCOAST <adusarkodielistowel@yahoo.com>
2023-12-14 16:31:47 -0800

    0x00. C - Hello, World Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - The main function
+ * main - Entry point of the program
  *
  * Return: Always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
