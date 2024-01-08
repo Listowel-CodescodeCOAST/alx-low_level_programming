ea85a7411d7eaaaded457a6e46033b81c7dd3832
Listowel-CodescodeCOAST <adusarkodielistowel@yahoo.com>
2024-01-08 14:26:34 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - constant memory byte
+ * @s: memory to be filled 
+ * @b: char to copy
+ * @ number of times to copy b
+ *
+ * Return: point to s memory 
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+	for (i = 0, i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
